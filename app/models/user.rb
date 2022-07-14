500a15a8309efd65482c588e2ca9d68f0725adbf
chucoyos <facturasmanzanillo@hotmail.com>
2022-07-14 11:45:48 -0500

    create message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 	has_secure_password
-	validates :username, presence: true, uniqueness: true
+	validates :username, presence: true, uniqueness: { case_sensitive: false }
 	validates :password, presence: true, length: { minimum: 6, maximum: 20 }
+	has_many :messages
 end
